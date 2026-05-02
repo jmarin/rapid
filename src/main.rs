9c7c843cddaf65e34092a38c5f7a38a4607503da
Juan Marin Otero <juan.marin.otero@gmail.com>
2026-05-02 09:22:43 -0400

    Add upload endpoint and basic UI

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use dotenvy::dotenv;
 use rapid::{AppState, Application, magic::mime_type_magic};
+use std::path::PathBuf;
 use std::{env, path::Path};
 use tracing::info;
 use tracing_subscriber::EnvFilter;
@@ -8,25 +9,26 @@ mod utils;
 
 use utils::*;
 
-use crate::utils::constants::prod::APP_ADDRESS;
+use crate::utils::constants::prod::{APP_ADDRESS, DEFAULT_LOG_LEVEL, DEFAULT_UPLOAD_DIR};
 
 // Temporary main. This will run the Axum web service
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     dotenv().ok();
 
-    let log_level = env::var("RAPID_LOG_LEVEL").unwrap_or_else(|_| "info".to_string());
+    let log_level = env::var("RAPID_LOG_LEVEL").unwrap_or_else(|_| DEFAULT_LOG_LEVEL.to_string());
     tracing_subscriber::fmt()
         .json()
         .with_env_filter(EnvFilter::new(log_level))
         .init();
 
-    //info!("Rapid starting up on port {}", http_port);
+    let upload_dir = match env::var("RAPID_UPLOAD_DIR") {
+        Ok(dir) => PathBuf::from(dir),
+        Err(_) => env::temp_dir().join(DEFAULT_UPLOAD_DIR),
+    };
+    tokio::fs::create_dir_all(&upload_dir).await?;
 
-    let mime_type = mime_type_magic(Path::new("data/files/NGC 6888.jpg")).await?;
-    info!("Detected MIME type: {}", mime_type);
-
-    let app_state = AppState {};
+    let app_state = AppState { upload_dir };
 
     let app = Application::build(app_state, APP_ADDRESS)
         .await
